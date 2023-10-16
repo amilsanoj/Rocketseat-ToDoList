8ea52acd3e136da1a39481b203c8d0a531fc9183
Jonas Lima Junior <jonasgomeslimajunior@gmail.com>
2023-10-16 14:43:53 -0300

    Up Application

---
+++ src/main/java/br/com/rocketseat/todoist/TodoistApplication.java
@@ -0,0 +1,15 @@
+package br.com.rocketseat.todoist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+public class TodoistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoistApplication.class, args);
+    }
+
+}
---
+++ src/main/java/br/com/rocketseat/todoist/filter/FilterTaskAuth.java
@@ -0,0 +1,53 @@
+package br.com.rocketseat.todoist.filter;
+
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.rocketseat.todoist.user.IUserRepository;
+import br.com.rocketseat.todoist.user.UserModel;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Base64;
+
+@Component
+public class FilterTaskAuth extends OncePerRequestFilter {
+
+    @Autowired
+    private IUserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String servletPath = request.getServletPath();
+        if (servletPath.equals("/tasks/")) {
+            //Autenticação
+            String authEncoded = request.getHeader("Authorization").substring("Basic".length()).trim();
+
+            byte[] authDecoded = Base64.getDecoder().decode(authEncoded);
+            String authString = new String(authDecoded);
+            String[] credentials = authString.split(":");
+            String username = credentials[0];
+            String password = credentials[1];
+
+
+            //Validação
+            UserModel user = this.userRepository.findByUsername(username);
+            if (user == null) {
+                response.sendError(401);
+            } else {
+                if (BCrypt.verifyer().verify(password.toCharArray(), userRepository.findByUsername(username).getPassword()).verified) {
+                    request.setAttribute("idUser", user.getId());
+                    filterChain.doFilter(request, response);
+                } else {
+                    response.sendError(401);
+                }
+            }
+        }else{
+            filterChain.doFilter(request, response);
+        }
+    }
+}
